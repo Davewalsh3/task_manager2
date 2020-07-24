6d67e70a439a97f288bf16ad0a820813f830ea4c
DavW <Davidpmwalsh@protonmail.com>
2020-07-24 15:24:14 +0000

    added app.route for /add_task page in app.py file

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html',
-                           tasks=mongo.db.tasks.find())
+    tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_tasks():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
