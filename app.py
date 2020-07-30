51c1119229070cfc5127a9c3a2aaf39cc999b0ff
DavW <Davidpmwalsh@protonmail.com>
2020-07-30 15:06:29 +0000

    Update Task in the database

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ from os import path
 if path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost")
 
 
 mongo = PyMongo(app)
 
-
+@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
     return render_template('tasks.html',
                            tasks=mongo.db.tasks.find())
 
 
-@app.route('/')
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
@@ -35,6 +32,27 @@ def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edittask.html', task=the_task,
+                           categories=all_categories)
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks')) 
 
 
 if __name__ == '__main__':
